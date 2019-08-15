bcb9c37db0c6e4e5c9f6fbc5f81360c8c4e31d6b
John Chan <johnchancontact@gmail.com>
2019-08-15 12:59:27 -0400

    deploying

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+  API_ENDPOINT: `https://desolate-earth-28222.herokuapp.com/`,
   API_TOKEN: process.env.REACT_API_TOKEN
-}
+};
